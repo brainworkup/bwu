b31662f8409d3ca8ff1f5f028898b26ee87e9c12
jtrampush <j.trampush@gmail.com>
2021-02-16 10:22:22 -0800

    commit changes after deleting old files

--- R/nest_tbl_domain.R
+++
@@ -1,8 +0,0 @@
-domain_z <- function(data = neurocog, domain = domain, z = z, mean, sd) {
-  domain_z <- neurocog %>%
-    group_by(domain) %>%
-    summarize(across(z, c(mean, sd)))
-}
-
-
-mtcars
--- fish_shell_commands.sh
+++
@@ -1,4 +0,0 @@
-# fish shell abbreviations
-abbr -a gco git checkout 
-
-set -U fish_user_paths $fish_user_paths /usr/local/bin
