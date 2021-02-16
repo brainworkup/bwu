570e111f88ef14b09b867524d038991b13172584
jtrampush <j.trampush@gmail.com>
2021-02-16 09:56:57 -0800

    Commit first R package ("npsych.data") 2021-02-16

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Dot Counting Task from NIH EXAMINER
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#'  diamonds. The variables are as follows:
+#' raw	age	percentile	n	m	md	sd	normValue
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{raw}{raw score}
+#'   \item{age}{age in years}
+#'   \item{percentile}{percentile rank}
+#'   \item{n}{sample size per group}
+#'   \item{m}{mean score}
+#'   \item{md}{median score}
+#'   \item{sd}{standard deviation of score}
+#'   \item{normValue}{T score fot dot counting task}
+#' }
+"dots"
