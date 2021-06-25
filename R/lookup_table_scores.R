79bb67c462e6938fcb7f9a0cee35f5eb922065c7
brainworkup <j.trampush@gmail.com>
2021-06-25 16:28:40 -0700

    commit some changes

---
+++ R/lookup_table_scores.R
@@ -0,0 +1,11 @@
+# Lookup Table - Neuropsych Scores
+
+score_standard <- c(>=130 = "Exceptionally High", 120:129 = "Above Average", 110:119 = "High-Average", 90:109 = "Average", 80:89 = "Low-Average", 70:79 = "Below Average", <70 = "Exceptionally Low")
+
+score_t <- c( >70 = "Exceptionally High", 63:69 = "Above Average", 57:62 = "High-Average", 44:56 = "Average", 37:43 = "Low-Average", 30:36 = "Below Average", <30 = "Exceptionally Low")
+
+score_scaled <- c(>16 = "Exceptionally High", 14:15 = "Above Average", 12:13= "High-Average", 9:11 = "Average", 7:8 = "Low-Average", 4:6 = "Below Average", <4 = "Exceptionally Low")
+
+score_z <- c(>2 = "Exceptionally High", 1.3:1.9 = "Above Average", 0.7:1.2 = "High-Average", -0.7:0.6 = "Average", -1.3:-0.6 = "Low-Average", -2:-1.4 = "Below Average", <=-2 = "Exceptionally Low")
+
+score_percentile <- c(>98 = "Exceptionally High", 91:97 = "Above Average", 75:90 = "High-Average", 25:74 = "Average", 9:24 = "Low-Average", 2:8 = "Below Average", <2 = "Exceptionally Low")
