f3a10519ff0e6d52daec950e7ca9bddba8d02f4b
Joey Trampush <j.trampush@gmail.com>
2022-03-12 20:56:01 -0800

    added $$ to funciton

--- R/make_tibble.R
+++ R/make_tibble.R
@@ -32,7 +32,7 @@ make_tibble <- function(tibb,
                         names = c(
                           "Scale",
                           "Score",
-                          "$&#x2030$ Rank",
+                          "&#x$2030$ Rank",
                           "Range",
                           "Subdomain",
                           "Test"
