ced9b0a3acedff53df1bb84295506c6bec092016
brainworkup <j.trampush@gmail.com>
2022-06-06 22:50:19 -0700

    BIG updates to NAB pluck .Rmd

--- inst/rmarkdown/templates/neuropsych_report_adult/skeleton/2.10_adhd.R
+++ inst/rmarkdown/templates/neuropsych_report_adult/skeleton/2.10_adhd.R
@@ -1,6 +1,6 @@
 ## ---- 01-filter-adhd ----
 filter_domain <- c(
-  # Brown
+  # Brown Scales
   "Activation",
   "Focus",
   "Effort",
@@ -9,15 +9,7 @@ filter_domain <- c(
   "Action",
   "Total Composite",
   # CAARS
-  "Inattention/Memory Problems",
-  "Hyperactivity/Restlessness",
-  "Impulsivity/Emotional Lability",
-  "Problems with Self-Concept",
-  "DSM-IV Inattentive Symptoms",
-  "DSM-IV Hyperactive-Impulsive Symptoms",
-  "DSM-IV ADHD Symptoms Total",
   "ADHD Index",
-  # CAARS updated MHS version
   "Inattention/Memory Problems",
   "Hyperactivity/Restlessness",
   "Impulsivity/Emotional Lability",
@@ -25,7 +17,6 @@ filter_domain <- c(
   "DSM-5 Inattentive Symptoms",
   "DSM-5 Hyperactive-Impulsive Symptoms",
   "DSM-5 ADHD Symptoms Total",
-  "ADHD Index",
   # CEFI
   "Full Scale",
   "Attention",
@@ -39,10 +30,11 @@ filter_domain <- c(
   "Working Memory"
 )
 
-## ---- 02-glue-adhd ------------
+## ---- 02-glue-adhd-sr ----
 dt <-
   neurobehav %>%
   dplyr::filter(scale %in% filter_domain) %>%
+  dplyr::filter(test == "cefi_sr" | test == "caars_sr") %>%
   dplyr::arrange(desc(percentile)) %>%
   dplyr::distinct(.keep_all = FALSE)
 
@@ -55,7 +47,24 @@ dt %>%
     append = TRUE
   )
 
-## ---- 03-table-adhd ------------
+## ---- 02-glue-adhd-or -----
+dt <-
+  neurobehav %>%
+  dplyr::filter(scale %in% filter_domain) %>%
+  dplyr::filter(test == "cefi_or" | test == "caars_or") %>%
+  dplyr::arrange(desc(percentile)) %>%
+  dplyr::distinct(.keep_all = FALSE)
+
+dt %>%
+  glue::glue_data() %>%
+  purrr::modify(lift(paste0)) %>%
+  cat(dt$result,
+    file = "2.10_adhd.md",
+    fill = TRUE,
+    append = TRUE
+  )
+
+## ---- 03-table-adhd ----
 tb <-
   bwu::make_tibble(
     tibb = adhd,
@@ -65,15 +74,15 @@ tb <-
   dplyr::filter(Scale %in% filter_domain) %>%
   dplyr::arrange(Test)
 
-## ---- 04-kable-adhd ------------------
+## ---- 04-kable-adhd ----
 kableExtra::kbl(
   tb[, 1:4], "latex",
   longtable = FALSE,
   booktabs = TRUE,
   linesep = "",
   align = c("lccc"),
-  caption = "(ref:caars) and the (ref:cefi)"
-  ) %>%
+  caption = "(ref:adhd-ef)"
+) %>%
   kableExtra::kable_paper(., lightable_options = "basic") %>%
   kableExtra::kable_styling(., latex_options = c(
     "scale_down", "HOLD_position", "striped"
@@ -83,7 +92,7 @@ kableExtra::kbl(
   kableExtra::row_spec(., row = 0, bold = TRUE) %>%
   kableExtra::add_footnote("(ref:fn-adhd)")
 
-## ---- 05-df-adhd -----------------------------------
+## ---- 05-df-adhd ----
 df <-
   neurobehav %>%
   dplyr::filter(domain == "Behavioral/Emotional/Social") %>%
@@ -91,7 +100,7 @@ df <-
   dplyr::filter(!is.na(percentile)) %>%
   dplyr::filter(filename %in% c("caars_sr.csv", "caars_or.csv"))
 
-## ---- 06-plot-adhd-adhd -------------------
+## ---- 06-plot-adhd-adhd ----
 bwu::dotplot(
   data = df,
   x = df$z_mean_narrow,
@@ -99,7 +108,7 @@ bwu::dotplot(
   domain = "attention/executive"
 )
 
-## ---- 07-df-adhd -----------------------------------
+## ---- 07-df-adhd ----
 df <-
   neurobehav %>%
   dplyr::filter(domain == "Behavioral/Emotional/Social") %>%
@@ -107,7 +116,7 @@ df <-
   dplyr::filter(!is.na(percentile)) %>%
   dplyr::filter(filename %in% c("cefi_sr.csv", "cefi_or.csv"))
 
-## ---- 08-plot-executive-adhd --------------------
+## ---- 08-plot-executive-adhd -----
 bwu::dotplot(
   data = df,
   x = df$z_mean_narrow,
