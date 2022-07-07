4da808be447a3dca69ca5601bcae2ccf33a72160
brainworkup <j.trampush@gmail.com>
2022-07-07 15:53:06 -0700

    Fixed (maybe) gpluck_t2z2p.R

--- R/gpluck_t2z2p.R
+++ R/gpluck_t2z2p.R
@@ -11,19 +11,19 @@
 gpluck_t2z2p <-
   function(...,
            table,
-           score,
+           score = NULL,
            score_type) {
     if (score_type == "t_score") {
       table <-
-        table |>
+        table %>%
         dplyr::mutate(z = (score - 50) / 10) %>%
-        dplyr::mutate(pct1 = round(stats::pnorm(z) * 100, 1)) |>
+        dplyr::mutate(pct1 = round(stats::pnorm(z) * 100, 1)) %>%
         dplyr::mutate(pct2 = dplyr::case_when(
           pct1 < 1 ~ ceiling(pct1),
           pct1 > 99 ~ floor(pct1),
           TRUE ~ round(pct1)
-        )) |>
-        dplyr::mutate(pct3 = pct2) |>
+        )) %>%
+        dplyr::mutate(pct3 = pct2) %>%
         dplyr::mutate(
           range = dplyr::case_when(
             pct3 >= 98 ~ "Exceptionally High",
@@ -35,36 +35,35 @@ gpluck_t2z2p <-
             pct3 < 2 ~ "Exceptionally Low",
             TRUE ~ as.character(range)
           )
-        ) |>
-        dplyr::mutate(percentile = pct1) |>
-        dplyr::mutate(result = glue::glue("{description} was {range}.")) |>
+        ) %>%
+        dplyr::mutate(percentile = pct1) %>%
+        dplyr::mutate(result = glue::glue("{description} was {range}.")) %>%
         dplyr::select(-c(z, pct1, pct2, pct3))
     } else if (score_type == "standard_score") {
-        table <-
-          table |>
-          dplyr::mutate(z = (score - 100) / 15) %>%
-          dplyr::mutate(pct1 = round(stats::pnorm(z) * 100, 1)) |>
-          dplyr::mutate(pct2 = dplyr::case_when(
-            pct1 < 1 ~ ceiling(pct1),
-            pct1 > 99 ~ floor(pct1),
-            TRUE ~ round(pct1)
-          )) |>
-          dplyr::mutate(pct3 = pct2) |>
-          dplyr::mutate(
-            range = dplyr::case_when(
-              pct3 >= 98 ~ "Exceptionally High",
-              pct3 %in% 91:97 ~ "Above Average",
-              pct3 %in% 75:90 ~ "High Average",
-              pct3 %in% 25:74 ~ "Average",
-              pct3 %in% 9:24 ~ "Low Average",
-              pct3 %in% 2:8 ~ "Below Average",
-              pct3 < 2 ~ "Exceptionally Low",
-              TRUE ~ as.character(range)
-            )
+      table <-
+        table %>%
+        dplyr::mutate(z = (score - 100) / 15) %>%
+        dplyr::mutate(pct1 = round(stats::pnorm(z) * 100, 1)) %>%
+        dplyr::mutate(pct2 = dplyr::case_when(
+          pct1 < 1 ~ ceiling(pct1),
+          pct1 > 99 ~ floor(pct1),
+          TRUE ~ round(pct1)
+        )) %>%
+        dplyr::mutate(pct3 = pct2) %>%
+        dplyr::mutate(
+          range = dplyr::case_when(
+            pct3 >= 98 ~ "Exceptionally High",
+            pct3 %in% 91:97 ~ "Above Average",
+            pct3 %in% 75:90 ~ "High Average",
+            pct3 %in% 25:74 ~ "Average",
+            pct3 %in% 9:24 ~ "Low Average",
+            pct3 %in% 2:8 ~ "Below Average",
+            pct3 < 2 ~ "Exceptionally Low",
+            TRUE ~ as.character(range)
           )
-        |>
-        dplyr::mutate(percentile = pct1) |>
-        dplyr::mutate(result = glue::glue("{description} was {range}.")) |>
+        ) %>%
+        dplyr::mutate(percentile = pct1) %>%
+        dplyr::mutate(result = glue::glue("{description} was {range}.")) %>%
         dplyr::select(-c(z, pct1, pct2, pct3))
-      } 
- }
+    }
+  }
