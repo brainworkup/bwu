97f228b2fbfdee4929b2e8c33660153bcba92a6e
brainworkup <j.trampush@gmail.com>
2022-07-17 02:06:05 -0700

    CHANGED ggplot2 theme (or trying to)

--- R/dotplot.R
+++ R/dotplot.R
@@ -5,6 +5,7 @@
 #' @param data Name of dataset
 #' @param x Name of x-axis variable
 #' @param y Name of y-axis variable
+#' @param theme Name of ggtheme
 #' @param ... Additional arguments
 #' @return A dotplot using ggplot2
 #'
@@ -12,7 +13,7 @@
 #' dotplot(iris, iris$Sepal.Length, iris$Species)
 #'
 #' @export
-dotplot <- function(data, x, y, ...) {
+dotplot <- function(data, x, y, theme = ggthemes::theme_fivethirtyeight(), ...) {
   ggplot2::ggplot() +
     ggplot2::geom_segment(
       data = data,
@@ -32,7 +33,7 @@ dotplot <- function(data, x, y, ...) {
       color = "black",
       fill = "orange"
     ) +
-    ggthemes::theme_fivethirtyeight() +
+    theme <- theme +
     ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white")) +
     ggplot2::theme(plot.background = ggplot2::element_rect(fill = "white")) +
     ggplot2::theme(panel.border = ggplot2::element_rect(color = "white"))
