0f2abdf84c35a113fdcd908510daef766e32badb
brainworkup <j.trampush@gmail.com>
2022-07-18 20:27:56 -0700

    fixed dotplot

--- R/dotplot.R
+++ R/dotplot.R
@@ -5,7 +5,6 @@
 #' @param data Name of dataset
 #' @param x Name of x-axis variable
 #' @param y Name of y-axis variable
-#' @param theme Name of ggtheme
 #' @param ... Additional arguments
 #' @return A dotplot using ggplot2
 #'
@@ -13,7 +12,7 @@
 #' dotplot(iris, iris$Sepal.Length, iris$Species)
 #'
 #' @export
-dotplot <- function(data, x, y, theme = ggthemes::theme_fivethirtyeight(), ...) {
+dotplot <- function(data, x, y, ...) {
   ggplot2::ggplot() +
     ggplot2::geom_segment(
       data = data,
@@ -33,7 +32,7 @@ dotplot <- function(data, x, y, theme = ggthemes::theme_fivethirtyeight(), ...) 
       color = "black",
       fill = "orange"
     ) +
-    theme <- theme +
+    ggthemes::theme_fivethirtyeight() +
     ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white")) +
     ggplot2::theme(plot.background = ggplot2::element_rect(fill = "white")) +
     ggplot2::theme(panel.border = ggplot2::element_rect(color = "white"))
