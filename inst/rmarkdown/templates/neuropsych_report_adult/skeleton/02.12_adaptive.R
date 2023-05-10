c65f3a165300cb9e88bc12e656427f2f4c0090a9
Joey Trampush <j.trampush@gmail.com>
2023-05-10 11:12:11 -0700

    caars cefi

--- inst/rmarkdown/templates/neuropsych_report_adult/skeleton/02.12_adaptive.R
+++ inst/rmarkdown/templates/neuropsych_report_adult/skeleton/02.12_adaptive.R
@@ -1,64 +1,24 @@
-## ---- 01-filter-emotion ---------
+## ---- 01-filter-adaptive -------------
 filter_domain <- c(
-  # PAI
-  "Somatic Complaints",
-  "Conversion",
-  "Somatization",
-  "Health Concerns",
-  "Anxiety",
-  "Cognitive (A)",
-  "Affective (A)",
-  "Physiological (A)",
-  "Anxiety-Related Disorders",
-  "Obsessive-Compulsive",
-  "Phobias",
-  "Traumatic Stress",
-  "Depression",
-  "Cognitive (D)",
-  "Affective (D)",
-  "Physiological (D)",
-  "Mania",
-  "Activity Level",
-  "Grandiosity",
-  "Irritability",
-  "Paranoia",
-  "Hypervigilance",
-  "Persecution",
-  "Resentment",
-  "Schizophrenia",
-  "Psychotic Experiences",
-  "Social Detachment",
-  "Thought Disorder",
-  "Borderline Features",
-  "Affective Instability",
-  "Identity Problems",
-  "Negative Relationships",
-  "Self-Harm",
-  "Antisocial Features",
-  "Antisocial Behaviors",
-  "Egocentricity",
-  "Stimulus-Seeking",
-  "Aggression",
-  "Aggressive Attitude",
-  "Verbal Aggression",
-  "Physical Aggression",
-  "Alcohol Problems",
-  "Drug Problems",
-  "Suicidal Ideation",
-  "Stress",
-  "Nonsupport",
-  "Treatment Rejection",
-  "Dominance",
-  "Warmth",
-  "ALC Estimated Score",
-  "DRG Estimated Score",
-  # BDI-2
-  "BID-2 Total Score",
-  # BAI-2
-  "BAI-2 Total Score"
+  ### adaptive
+  "General Adaptive Composite",
+  "Conceptual Skills Index",
+  "Communication",
+  "Functional Academics",
+  "Self-Direction",
+  "adaptive Skills Index",
+  "Leisure",
+  "adaptive",
+  "Practical Skills Index",
+  "Community Use",
+  "School Living",
+  "Home Living",
+  "Health and Safety",
+  "Self-Care",
+  "Work"
 )
 
-## ---- 02-glue-emotion ------------
+## ---- 02-glue-adaptive ------------
 dt <-
   neurobehav |>
   tidytable::filter(scale %in% filter_domain) |>
@@ -69,70 +29,30 @@ dt |>
   glue::glue_data() |>
   purrr::modify(purrr::lift(paste0)) |>
   cat(dt$result,
-    file = "02.11_emotion.md",
+    file = "02.09_adaptive.md",
     fill = TRUE,
     append = TRUE
   )
 
-## ---- 03-table-axis1 ------------
-tb1 <-
-  bwu::make_tibble(
-    tibb = tb1,
-    data = neurobehav,
-    pheno = "Psychiatric Disorders"
-  ) |>
-  tidytable::filter(Scale %in% filter_domain) |>
-  tidytable::arrange(Test) |>
-  tidytable::arrange(Subdomain)
-
-## ---- 03-table-axis2 ------------
-tb2 <-
-  bwu::make_tibble(
-    tibb = tb2,
-    data = neurobehav,
-    pheno = "Personality Disorders"
-  ) |>
-  tidytable::filter(Scale %in% filter_domain) |>
-  tidytable::arrange(Test) |>
-  tidytable::arrange(Subdomain)
-
-## ---- 03-table-sud ------------
-tb3 <-
-  bwu::make_tibble(
-    tibb = tb3,
-    data = neurobehav,
-    pheno = "Substance Use"
-  ) |>
-  tidytable::filter(Scale %in% filter_domain) |>
-  tidytable::arrange(Test) |>
-  tidytable::arrange(Subdomain)
-
-## ---- 03-table-social ------------
-tb4 <-
-  bwu::make_tibble(
-    tibb = tb4,
-    data = neurobehav,
-    pheno = "Psychosocial Problems"
-  ) |>
-  tidytable::filter(Scale %in% filter_domain) |>
-  tidytable::arrange(Test) |>
-  tidytable::arrange(Subdomain)
-
-## ---- 03-table-emotion ------------
+## ---- 03-table-adaptive ------------
 tb <-
-  tidytable::bind_rows(tb1, tb2, tb3) |>
+  bwu::make_tibble(
+    tibb = adaptive,
+    data = neurobehav,
+    pheno = "Adaptive Functioning"
+  ) |>
   tidytable::filter(Scale %in% filter_domain) |>
-  tidytable::arrange(Test) |>
-  tidytable::arrange(Subdomain)
+  tidytable::arrange(Test)
 
-## ---- 04-kable-emotion ------------------
+## ---- 04-kable-adaptive ------------------
 kableExtra::kbl(
-  tb[, 1:4], "latex",
+  tb[, 1:4],
+  "latex",
   longtable = FALSE,
   booktabs = TRUE,
   linesep = "",
   align = c("lccc"),
-  caption = "(ref:emotion)"
+  caption = "(ref:adaptive)"
 ) |>
   kableExtra::kable_paper(lightable_options = "basic") |>
   kableExtra::kable_styling(latex_options = c(
@@ -142,28 +62,29 @@ kableExtra::kbl(
   )) |>
   kableExtra::column_spec(1, width = "8cm") |>
   kableExtra::pack_rows(index = table(tb$Test)) |>
-  kableExtra::pack_rows(index = table(tb$Subdomain)) |>
   kableExtra::row_spec(row = 0, bold = TRUE) |>
-  kableExtra::add_footnote("(ref:fn-emo)")
+  kableExtra::add_footnote("(ref:fn-adaptive)")
 
-## ---- 05-df-emotion -----------------------------------
+## ---- 05-df-adaptive ------------
 df <-
   neurobehav |>
+  tidytable::filter(domain == "Adaptive Functioning") |>
   tidytable::filter(!is.na(percentile)) |>
+  tidytable::arrange(test_name) |>
   tidytable::filter(scale %in% filter_domain)
 
-## ---- 06-plot-subdomain-emotion -------------------
+## ---- 06-plot-subdomain-adaptive -----------------
 bwu::dotplot(
   data = df,
   x = df$z_mean_sub,
   y = df$subdomain,
-  domain = "emotion"
+  domain = "adaptive"
 )
 
-## ---- 07-plot-narrow-emotion --------------------
+## ---- 07-plot-narrow-adaptive -------------------
 bwu::dotplot(
   data = df,
   x = df$z_mean_narrow,
   y = df$narrow,
-  domain = "emotion"
+  domain = "adaptive"
 )
