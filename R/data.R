c414b4549c1c6c9843c8b654f5c70fb7b38d201a
Joey Trampush <j.trampush@gmail.com>
2023-08-30 00:34:50 -0700

    ADDED datasets for neuropsych scoring as internal in R/sysdata.rda

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Dot Counting Task from NIH EXAMINER
 #' A dataset containing the prices and other attributes of almost 54,000
 #'  diamonds. The variables are as follows:
-#' raw	age	percentile	n	m	md	sd	normValue
+#' raw age percentile n m md sd normValue
 #' @format A data frame with 53940 rows and 10 variables:
 #' \describe{
 #'   \item{raw}{raw score}
