229a58e6e808508d15e724b44acdb64414ce4845
Joey Trampush <j.trampush@gmail.com>
2023-09-22 22:19:00 -0700

    not even sure

---
+++ inst/rmarkdown/templates/neuropsych_report/skeleton/run_scrape_pdf_nse.R
@@ -0,0 +1,24 @@
+# R Script for NeuroPsych
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 1) {
+  stop("Usage: run_scrape_pdf_nse.R <patient>")
+}
+
+patient <- args[1]
+
+# caars_sr
+rmarkdown::render("pluck_caars.Rmd", params = "ask")
+Sys.sleep(1)
+
+# caars_or
+rmarkdown::render("pluck_caars.Rmd", params = "ask")
+Sys.sleep(1)
+
+# cefi_sr
+rmarkdown::render("pluck_cefi.Rmd", params = "ask")
+Sys.sleep(1)
+
+# cefi or
+rmarkdown::render("pluck_cefi.Rmd", params = "ask")
+Sys.sleep(1)
