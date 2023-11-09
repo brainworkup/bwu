695badc59760969166f96fe94cd9a6876efd90af
Joey Trampush <j.trampush@gmail.com>
2023-11-09 05:54:30 -0800

    fixes to rocft fx and plant_csv

--- R/neuropsych_test_scoring.R
+++ R/neuropsych_test_scoring.R
@@ -66,8 +66,8 @@ tmt_b <- function(raw_score, age) {
 
 #' @title Rey Complex Figure, Copy Trial Score Lookup
 #' @description Calculates a Rey Complex Figure, Copy trial score (T-score) from a raw score and age. The predicted score and standard deviation are also calculated.
-#' @param raw_score A numeric value of the raw score from the Rey Complex Figure, Copy trial.
 #' @param age A numeric value of the age of the participant. Must be between 16 and 89 years old.
+#' @param raw_score A numeric value of the raw score from the Rey Complex Figure, Copy trial.
 #' @return Returns a numeric value of the T-score for the Rey Complex Figure, Copy trial.
 #' @details This function takes the raw score and age of a participant and calculates the Rey Complex Figure, Copy trial T-score using the formula provided by \emph{Manly et al. (2000)}. It also returns the predicted score and standard deviation for the trial.
 #' @references Manly, J.J., Schinka, J.A., & Guerrero, L. (2000). Normative Data on the Rey Complex Figure Test in Older Adults. Archives of Clinical Neuropsychology, 15(7), 613-621.
@@ -101,11 +101,10 @@ rocft_copy_t_score <- function(age, raw_score) {
     if (age > MAX_AGE) {
       return(paste0("Age must be ", MAX_AGE, " or younger"))
     }
-
     return(NULL)
   }
 
-  validation_message <- validate_inputs(raw_score, age)
+  validation_message <- validate_inputs(age, raw_score)
   if (!is.null(validation_message)) {
     return(validation_message)
   }
@@ -125,7 +124,7 @@ rocft_copy_t_score <- function(age, raw_score) {
   predicted_score <- round(predicted_score, digits = 2)
   predicted_sd <- round(predicted_sd, digits = 2)
 
-  paste0(
+  return(paste0(
     "ROCFT Copy T-Score: ",
     t_score,
     ", z-Score = ",
@@ -134,16 +133,13 @@ rocft_copy_t_score <- function(age, raw_score) {
     predicted_score,
     ", Predicted SD = ",
     predicted_sd
-  )
-  return(cat(t_score))
+  ))
 }
 
-
-
-#' @title Rey Complex Figure, Delayed Recall Score Lookup
+#' @title Rey Complex Figure, Long-Term Delayed Recall Score Lookup
 #' @description Calculates a Rey Complex Figure, Delayed Recall (30-min) trial score (T-score) from a raw score and age. The predicted score and standard deviation are also calculated.
-#' @param raw_score A numeric value of the raw score from the Rey Complex Figure, Copy trial.
 #' @param age A numeric value of the age of the participant. Must be between 16 and 89 years old.
+#' @param raw_score A numeric value of the raw score from the Rey Complex Figure, Copy trial.
 #' @return Returns a numeric value of the T-score for the Rey Complex Figure, Copy trial.
 #' @details This function takes the raw score and age of a participant and calculates the Rey Complex Figure, Copy trial T-score using the formula provided by \emph{Manly et al. (2000)}. It also returns the predicted score and standard deviation for the trial.
 #' @references Manly, J.J., Schinka, J.A., & Guerrero, L. (2000). Normative Data on the Rey Complex Figure Test in Older Adults. Archives of Clinical Neuropsychology, 15(7), 613-621.
@@ -181,18 +177,9 @@ rocft_recall_t_score <- function(age, raw_score) {
     return(NULL)
   }
 
-  validation_message <- validate_inputs(raw_score, age)
+  validation_message <- validate_inputs(age, raw_score)
   if (!is.null(validation_message)) {
     return(validation_message)
-  }
-
-
-  if (age < 18) {
-    return("Age must be 16 or older")
-  }
-
-  if (age > 79) {
-    return("Age must be 89 or younger")
   }
 
   predicted_score <-
@@ -210,7 +197,7 @@ rocft_recall_t_score <- function(age, raw_score) {
   predicted_score <- round(predicted_score, digits = 2)
   predicted_sd <- round(predicted_sd, digits = 2)
 
-  paste0(
+  return(paste0(
     "ROCFT Delayed Recall T-Score: ",
     t_score,
     ", z-Score = ",
@@ -219,6 +206,5 @@ rocft_recall_t_score <- function(age, raw_score) {
     predicted_score,
     ", Predicted SD = ",
     predicted_sd
-  )
-  return(cat(t_score))
+  ))
 }
