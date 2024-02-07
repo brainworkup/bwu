aabf100fc695f9e5cc3922bf73995f3d25b13091
Joey Trampush <j.trampush@gmail.com>
2024-02-06 21:50:20 -0800

    data_load v2

--- R/wrangle_data.R
+++ R/wrangle_data.R
@@ -147,3 +147,113 @@ filter_data <- function(data, domain, scale) {
     dplyr::filter(scale %in% scales)
   return(data)
 }
+
+
+#' @title Read/Load Neuropsych Eval CSV Files 2
+#' @description This function reads .csv files of patient data and writes four different files of the same data that are categorized by neuropsychological test type.
+#' @importFrom here here
+#' @importFrom dplyr filter distinct mutate group_by ungroup
+#' @importFrom purrr map set_names list_rbind
+#' @importFrom forcats as_factor
+#' @importFrom stats qnorm
+#' @importFrom readr read_csv write_csv
+#' @param patient character, Name of patient, e.g. "Biggie"
+#' @return List with 4 elements, each element is a dataframe of patient data
+#' @rdname load_data
+#' @export
+load_data2 <- function() {
+  data_path <- here::here("csv")
+  files <- dir(data_path, pattern = "*.csv", full.names = TRUE)
+
+  neuropsych <-
+    purrr::map_df(files, function(filename) {
+      data <- readr::read_csv(filename, na = c("", "NA", "--", "-"))
+      data$filename <- basename(filename)
+      data
+    }) |>
+    dplyr::distinct() |>
+    (\(df) {
+      df <- dplyr::mutate(
+        df,
+        z = ifelse("percentile" %in% names(df), qnorm(df$percentile / 100), NA_real_),
+        domain = as.character(df$domain),
+        subdomain = as.character(df$subdomain),
+        narrow = as.character(df$narrow),
+        pass = as.character(df$pass),
+        verbal = forcats::as_factor(df$verbal),
+        timed = forcats::as_factor(df$timed)
+      )
+      df
+    })()
+
+  # Subset neurocognitive data
+  neurocog <-
+    neuropsych |>
+    dplyr::filter(test_type == "npsych_test") |>
+    # domain
+    dplyr::group_by(domain, .add = TRUE) |>
+    dplyr::mutate(z_mean_domain = mean(z), z_sd_domain = sd(z)) |>
+    dplyr::ungroup() |>
+    # subdomain
+    dplyr::group_by(subdomain, .add = TRUE) |>
+    dplyr::mutate(z_mean_subdomain = mean(z), z_sd_subdomain = sd(z)) |>
+    dplyr::ungroup() |>
+    # narrow
+    dplyr::group_by(narrow, .add = TRUE) |>
+    dplyr::mutate(z_mean_narrow = mean(z), z_sd_narrow = sd(z)) |>
+    dplyr::ungroup() |>
+    # pass
+    dplyr::group_by(pass, .add = TRUE) |>
+    dplyr::mutate(z_mean_pass = mean(z), z_sd_pass = sd(z)) |>
+    dplyr::ungroup() |>
+    # verbal
+    dplyr::group_by(verbal, .add = TRUE) |>
+    dplyr::mutate(z_mean_verbal = mean(z), z_sd_verbal = sd(z)) |>
+    dplyr::ungroup() |>
+    # timed
+    dplyr::group_by(timed, .add = TRUE) |>
+    dplyr::mutate(z_mean_timed = mean(z), z_sd_timed = sd(z)) |>
+    dplyr::ungroup()
+
+  # Subset neurobehavioral data
+  neurobehav <-
+    neuropsych |>
+    dplyr::filter(test_type == "rating_scale") |>
+    # domain
+    dplyr::group_by(domain, .add = TRUE) |>
+    dplyr::mutate(z_mean_domain = mean(z), z_sd_domain = sd(z)) |>
+    dplyr::ungroup() |>
+    # subdomain
+    dplyr::group_by(subdomain, .add = TRUE) |>
+    dplyr::mutate(z_mean_subdomain = mean(z), z_sd_subdomain = sd(z)) |>
+    dplyr::ungroup() |>
+    # narrow
+    dplyr::group_by(narrow, .add = TRUE) |>
+    dplyr::mutate(z_mean_narrow = mean(z), z_sd_narrow = sd(z)) |>
+    dplyr::ungroup()
+
+  # Subset validity data
+  validity <-
+    neuropsych |>
+    dplyr::filter(
+      test_type %in% c("performance_validity", "symptom_validity")
+    ) |>
+    # domain
+    dplyr::group_by(domain, .add = TRUE) |>
+    dplyr::mutate(z_mean_domain = mean(z), z_sd_domain = sd(z)) |>
+    dplyr::ungroup() |>
+    # subdomain
+    dplyr::group_by(subdomain, .add = TRUE) |>
+    dplyr::mutate(z_mean_subdomain = mean(z), z_sd_subdomain = sd(z)) |>
+    dplyr::ungroup() |>
+    # narrow
+    dplyr::group_by(narrow, .add = TRUE) |>
+    dplyr::mutate(z_mean_narrow = mean(z), z_sd_narrow = sd(z)) |>
+    dplyr::ungroup()
+
+  # Write processed data to CSVs
+  readr::write_csv(neuropsych, here::here("neuropsych.csv"))
+  readr::write_csv(neurocog, here::here("neurocog.csv"))
+  readr::write_csv(neurobehav, here::here("neurobehav.csv"))
+  readr::write_csv(validity, here::here("validity.csv"))
+}
